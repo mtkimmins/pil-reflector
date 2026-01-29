afd7337c17ac1244ad87df324f7ba8e947450920
M. T. Kimmins <82971010+mtkimmins@users.noreply.github.com>
2026-01-28 21:04:40 -0700

    Update event listeners and implement local storage
    Refactor event listeners and add local storage functionality.

--- scripts.js
+++ scripts.js
@@ -14,44 +14,77 @@ const OUTPUT = document.getElementById("compile_body");
 
 
 
-CLEAR_BUTTON.addEventListener("click", wipe);
-COMPILE_BUTTON.addEventListener("click",compile);
-
-
+/////////////////////////////////////
+//  CLASSES/FUNCTIONS
+/////////////////////////////////////
 function wipe(){
   for (inp in input_list){
     inp.value = "";
+  }
   OUTPUT.value = "";
-  CLEAR_BUTTON.textContent("CLEARED!!")
-    }
+  CLEAR_BUTTON.textContent("CLEARED!!");
+  setTimeout(resetButton, 3000);
 }
 
 function compile(){
   let compile_text = ""
   for (inp in input_list){
     compile_text += inp.value
+    compile_text += "\n"
   }
-  OUTPUT.innerHTML=compile_text;
+  OUTPUT.value = compile_text;
   //Copy to clipboard
   OUTPUT.select();
   OUTPUT.setSelectionRange(0,99999);
   navigator.clipboard.writeText(compile_text);
-  COMPILE_BUTTON.textContent("COMPILED & COPIED!!")
+  //Visual feedback
+  COMPILE_BUTTON.textContent("COMPILED & COPIED!!");
+  setTimeout(resetButton,3000);
 }
-
-setInterval(saveData, 60000);
-setInterval(resetButton, 3000);
 
 function resetButton(){
   CLEAR_BUTTON.textContent("Clear");
   COMPILE_BUTTON.textContent("Compile");
 }
 
-function initializeData(){
-  }
 function saveData(){
-  }
-
+  localStorage.setItem("i1",INPUT_1.value);
+  localStorage.setItem("i2",INPUT_2.value);
+  localStorage.setItem("i3",INPUT_3.value);
+  localStorage.setItem("i4",INPUT_4.value);
+  localStorage.setItem("i5",INPUT_5.value);
+  localStorage.setItem("i6",INPUT_6.value);
+}
 
 function loadData(){
+  if (localStorage.getItem("i1") != null){
+    INPUT_1.value = localStorage.getItem("i1");
+  }
+  if (localStorage.getItem("i2") != null){
+    INPUT_2.value = localStorage.getItem("i2");
+  }
+  if (localStorage.getItem("i3") != null){
+    INPUT_3.value = localStorage.getItem("i3");
+  }
+  if (localStorage.getItem("i4") != null){
+    INPUT_4.value = localStorage.getItem("i4");
+  }
+  if (localStorage.getItem("i5") != null){
+    INPUT_5.value = localStorage.getItem("i5");
+  }
+  if (localStorage.getItem("i6") != null){
+    INPUT_6.value = localStorage.getItem("i6");
+  }
+  if (localStorage.getItem("out") != null){
+    OUTPUT.value = localStorage.getItem("out");
+  }
 }
+
+/////////////////////////////////////////
+//  RUNTIME
+/////////////////////////////////////////
+document.addEventListener("DOMContentLoaded", loadData);
+CLEAR_BUTTON.addEventListener("click", wipe);
+COMPILE_BUTTON.addEventListener("click",compile);
+
+setInterval(saveData, 60000);
